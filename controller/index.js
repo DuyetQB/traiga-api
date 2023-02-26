b4af1a5a0d78ce69ac58ff21842cacf5f57ac207
DESKTOP-RONIHRQ\Legion 5 <dinhsyduyet@gmail.com>
2023-02-26 21:56:50 +0700

    fix update route

--- controller/index.js
+++ controller/index.js
@@ -1,23 +1,13 @@
 const ModelBlog = require("../model/blog");
- const  { handleSlugString } = require("../utils/utils");
+const { handleSlugString } = require("../utils/utils");
 
 const getAllProduct = async (req, res) => {
-  
   try {
     const perPage = 10;
     const page = Math.max(0, req.query.page);
-    const datasResponse = await ModelBlog.find({}).limit(perPage).skip(perPage * page);
-
-  return res.status(200).json({ data: datasResponse, statusMessage: "ok" });
-  } catch (error) {
-    console.log("err:", error);
-  }
-};
-
-const getProductById = async (req, res) => {
-  try {
-
-    const datasResponse = await ModelBlog.findOne({slug:req.params.slug});
+    const datasResponse = await ModelBlog.find({})
+      .limit(perPage)
+      .skip(perPage * page);
 
     return res.status(200).json({ data: datasResponse, statusMessage: "ok" });
   } catch (error) {
@@ -25,20 +15,36 @@ const getProductById = async (req, res) => {
   }
 };
 
+const getProductById = async (req, res) => {
+  try {
+    const datasResponse = await ModelBlog.findOne({ slug: req.params.slug });
+
+    return res.status(200).json({ data: datasResponse, statusMessage: "ok" });
+  } catch (error) {
+    console.log("err:", error);
+  }
+};
 
 const createProduct = async (req, res) => {
   try {
-    const { title, body , author , description , imageUrl , imageThumbnailUrl,keyWords } = req.body;
+    const {
+      title,
+      body,
+      author,
+      description,
+      imageUrl,
+      keyWords,
+    } = req.body;
     const handleSlug = handleSlugString(title);
-    
+
     const datasCreate = new ModelBlog({
-      author:author,
+      author: author,
       title: title,
-      description:description,
+      description: description,
       body: body,
-      imageUrl:imageUrl,
-      slug:handleSlug,
-      keyWords:keyWords
+      imageUrl: imageUrl,
+      slug: handleSlug,
+      keyWords: keyWords,
     });
 
     datasCreate.save();
@@ -50,21 +56,18 @@ const createProduct = async (req, res) => {
 
 const updateProduct = async (req, res) => {
   try {
-    const { _id, ...other} = req.body;
-    let object = {
-      ...other
-    }
-    
-    if(other.title !== undefined) {
-      object = {
-        slug:handleSlugString(other.title),
-        ...other
-      }
-    }
-    
-    const dataUpdate = await ModelBlog.findByIdAndUpdate(_id,object);
+    const { _id, title } = req.body;
 
-    dataUpdate.save();
+    let newObject = {
+      ...req.body,
+      slug: handleSlugString(title),
+    };
+
+    const dataUpdate = await ModelBlog.findByIdAndUpdate(_id, newObject, {
+      new: true,
+    });
+
+    await dataUpdate.save();
     return res
       .status(201)
       .json({ data: dataUpdate, statusMessage: "Update success" });
@@ -73,37 +76,28 @@ const updateProduct = async (req, res) => {
 
 const deleteProductById = async (req, res) => {
   try {
+    const datasDeleteById = await ModelBlog.findByIdAndDelete(req.params.id);
+    if (!datasDeleteById)
+      return res.status(404).json({ statusMessage: "not found" });
 
-    const datasDeleteById = await ModelBlog.findByIdAndDelete(req.params.id)
-    if (!datasDeleteById) return res
-    .status(404)
-    .json({statusMessage: "not found" });
-
-    return res
-      .status(201)
-      .json({statusMessage: "delete success" });
-   
-  } catch (error) {
-
-  }
+    return res.status(201).json({ statusMessage: "delete success" });
+  } catch (error) {}
 };
 
 const deleteProductAll = async (req, res) => {
   try {
-    const datasDelete = ModelBlog.collection.deleteMany()
-   return res
-      .status(201)
-      .json({ statusMessage: "delete all datas success" });
+    const datasDelete = ModelBlog.collection.deleteMany();
+    return res.status(201).json({ statusMessage: "delete all datas success" });
   } catch (error) {}
 };
 
 const findProductByName = async (req, res) => {
   try {
     // const datasDelete = ModelBlog.
-    console.log("req:",req.body.query);
-  //  return res
-  //     .status(201)
-  //     .json({ data: datasDelete, statusMessage: "delete all datas success" });
+    console.log("req:", req.body.query);
+    //  return res
+    //     .status(201)
+    //     .json({ data: datasDelete, statusMessage: "delete all datas success" });
   } catch (error) {}
 };
 
